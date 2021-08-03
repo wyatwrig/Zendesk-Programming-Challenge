4bd9847969af44653094cc8f5dc89cf4403ce41b
wyatwrig <wyatwrig@gmail.com>
2021-08-03 09:29:35 -0600

    major auth & paging changes. Added error checks

--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
 import requests 
+import sys
 
 """
 This class handles manages the interactions between the user and 
@@ -9,10 +10,10 @@ class Manager:
     
     # user info variables
 
-    username = ''
-    user_in = ''
-    api_token = ''
-    web_address = "https://zccwright.zendesk.com" # <-- Change this address to work with other zendesk support profiles. Also update the user_info.txt file.
+    username = ""
+    user_in = ""
+    api_token = ""
+    web_address = "" 
     authenticated = False
 
     # pagination variables keep track of current page.
@@ -21,87 +22,101 @@ class Manager:
 
     def __init__(self, tl):
         print("Welcome to the Zendesk CLI ticket viewer")
-        self.authenticate()
-
-        # setting up initial page
-
-        # get total tickets
-        i = requests.get("https://zccwright.zendesk.com/api/v2/tickets/count", auth=(self.user_in, self.api_token))
-        tc_response = i.json()
-        ticket_count = int(tc_response["count"]["value"])
-
-        # if tickets > 25, set up for pagination
-        if ticket_count > 25:
-            payload = {"page[size]" : "25"}
-            r = requests.get("https://zccwright.zendesk.com/api/v2/tickets", auth=(self.user_in, self.api_token), params=payload)
-            response = r.json()
-            self.cursor_next = response["links"]["next"]
-            self.cursor_previous = response["links"]["prev"]
-            tl.parse_page(response)
-            tl.print_page()
-        # else, print all tickets
-        else:
-            r = requests.get("https://zccwright.zendesk.com/api/v2/tickets", auth=(self.user_in, self.api_token))
-            response = r.json()
-            tl.parse_page(response)
-            tl.print_page()
-    
-    # establishes the user's connection to the API
-    def authenticate(self):
-
-        while self.authenticated == False:
-
-            """ 
+        
+        """ 
             This block pulls the email and API token for the user from the user_info.txt file.
             This may be an unnecessary feature but I included it to avoid pushing sensitive info to a 
             public git repo where I'm hosting this project.
             """
-            with open("user_info.txt", 'r') as info_file:
-                holdinglist = info_file.read().splitlines()
-                self.username = holdinglist[0]
-                self.api_token = holdinglist[1]
+        if self.authenticated == False:
+            try:
+                with open("user_info.txt", 'r') as info_file:
+                    holdinglist = info_file.read().splitlines()
+                    self.username = holdinglist[0]
+                    self.api_token = holdinglist[1]
+                    self.web_address = holdinglist[2]
 
-            self.user_in = self.username + "/token" 
-
-            # Establish a connection with the API and verify user credentials by querying job statuses
-            r = requests.get("https://zccwright.zendesk.com/api/v2/job_statuses", auth=(self.user_in, self.api_token))
-
-            # Check the status code to verify good connection
-            if r.status_code != 200:
-                print(f"Problem with connection. Status code: {r.status_code}")
-            else:
-                print("Connection to API successful")
+                self.user_in = self.username + "/token"
                 self.authenticated = True
+            except OSError as e:
+                print("File user_info.txt not found")
+                print("Ensure file exists and re-run program")
+                sys.exit()
+
+        # setting up initial page
+        ticket_count = self.get_count()
+        tl.total_ticket_count = ticket_count
         
+
+        # if tickets > 25, set up for pagination
+        if ticket_count > 25:
+            payload = {"page[size]" : "25"}
+            response = self.query_api(f"{self.web_address}/api/v2/tickets", self.user_in, self.api_token, payload)
+            self.update_cursors(response["links"]["next"], response["links"]["prev"])
+            tl.parse_page(response)
+            tl.print_page()
+        # else, print all tickets
+        else:
+            response = self.query_api(f"{self.web_address}/api/v2/tickets", self.user_in, self.api_token)
+            tl.parse_page(response)
+            tl.print_page()
+       
 
     # provides pagination functionality
     def page(self, tl, page_token):
         if page_token == '+':
-            r = requests.get(self.cursor_next, auth=(self.user_in, self.api_token))
-            response = r.json()
+            response = self.query_api(self.cursor_next, self.user_in, self.api_token)
             tl.parse_page(response)
             tl.print_page()
-            link_forward = response["links"]["next"]
-            link_backwards = response["links"]["prev"]
-            if link_forward != None:
-                self.cursor_next = link_forward
-                self.cursor_previous = link_backwards
-            else:
-                print("End of forwards pagination")
+            self.update_cursors(response["links"]["next"], response["links"]["prev"])
                 
         elif page_token == '-':
-            r = requests.get(self.cursor_previous, auth=(self.user_in, self.api_token))
-            response = r.json()
+            response = self.query_api(self.cursor_previous, self.user_in, self.api_token)
             tl.parse_page(response)
             tl.print_page()
-            link_forward = response["links"]["next"]
-            link_backwards = response["links"]["prev"]
-            if link_forward != None:
-                self.cursor_next = link_forward
-                self.cursor_previous = link_backwards
-            else:
-                print("End of backwards pagination")
+            self.update_cursors(response["links"]["next"], response["links"]["prev"])
+    
+    def get_count(self):
+        # get total tickets
+        try:
+            i = requests.get(f"{self.web_address}/api/v2/tickets/count", auth=(self.user_in, self.api_token))
+            if i.status_code != 200:
+                print("Problem contacting Zendesk API")
+                print(f"Response Status: {i.status_code}")
+                error = i.json()["error"]
+                print(f"Error response from Zendesk: {error}")
+                sys.exit()
+            tc_response = i.json()
+            ticket_count = int(tc_response["count"]["value"])
+            return ticket_count
+        except requests.exceptions.RequestException as e:
+            print("Encountered a problem with your internet connection")
+            print("Ensure connection and re-run program")
+            sys.exit()
 
+
+    def query_api(self, url, user_in, api_token, payload = None):
+        try:
+            r = requests.get(url, auth=(user_in, api_token), params=payload)
+            if r.status_code != 200:
+                print("Problem contacting Zendesk API")
+                print(f"Response Status: {r.status_code}")
+                error = r.json()["error"]
+                print(f"Error response from Zendesk: {error}")
+                sys.exit()
+            response = r.json() 
+            return response
+        except requests.exceptions.RequestException as e:
+            print("Encountered a problem with your internet connection")
+            print("Ensure connection and re-run program")
+            sys.exit()
+
+    def update_cursors(self, forwards, backwards):
+        if forwards and backwards != None:
+            self.cursor_next = forwards
+            self.cursor_previous = backwards
+        else:
+            print("Can't page beyond bounds")
         
 
     # displays one specifc ticket from the tickets dictionary
@@ -111,9 +126,11 @@ class Manager:
     
     # prints the menu
     def print_menu(self):
+        print("=============================")
         print("List of commands: ")
         print("+ : view next page")
         print("- : view previous page")
         print("view : view specific ticket")
         print("help : display this menu")
         print("quit : exit the program")
+        print("=============================")
--- translator.py
+++ translator.py
@@ -1,5 +1,3 @@
-import json
-
 """
 This class serves as a mechanism to transform the responses from 
 the Zendesk API from JSON to Python data structures for easy access and
@@ -10,6 +8,7 @@ class Translator:
 
     # {'ticket_id': {ticket}}
     current_page_dict = {}
+    total_ticket_count = 0
     
     def __init__(self):
         pass
@@ -21,22 +20,35 @@ class Translator:
         for ticket in tickets_list:
             self.current_page_dict.update({ticket["id"] : ticket})
 
-    # prints the current page based on the contents of the current_page_dict
+    # prints a list of tickets based on the contents of the current_page_dict
     def print_page(self):
         for ticket in self.current_page_dict.values():
             id = ticket["id"]
             status = ticket["status"]
             subject = ticket["subject"]
             priority = ticket["priority"]
-            print(f"{status} ticket {id} with priority {priority} has subject: {subject}")
+            print(f"| {status} ticket {id} with priority {priority} has subject: {subject} |")
+        try:
+            first = list(self.current_page_dict.keys())[0]
+            second = list(self.current_page_dict.keys())[-1]
+            print(f"Viewing tickets {first} - {second} of {self.total_ticket_count}")
+        except IndexError:
+            print("Page in the other direction")
 
     # prints the ticket details as well as the description for an individual ticket
     def print_ticket(self, ticket_id):
         ticket = self.current_page_dict.get(int(ticket_id))
-        id = ticket["id"]
-        status = ticket["status"]
-        subject = ticket["subject"]
-        priority = ticket["priority"]
-        description = ticket["description"]
-        print(f"{status} ticket {id} with priority {priority} has subject: {subject}")
-        print(f"Ticket description: {description}")
+        try:
+            id = ticket["id"]
+            status = ticket["status"]
+            subject = ticket["subject"]
+            priority = ticket["priority"]
+            description = ticket["description"]
+            print(f"| {status} ticket {id} with priority {priority} has subject: {subject} |")
+            print("=============================")
+            print(f"Ticket description: {description}")
+            print("=============================")
+        except TypeError:
+            print("Ticket not found.")
+            print("Make sure the ticket you're trying to access is on the current page.")
+
